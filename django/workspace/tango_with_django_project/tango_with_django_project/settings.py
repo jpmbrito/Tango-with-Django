75a78a6141cd8c4078ad218780df8cd0fde0b6ff
Joao Brito <joao.mano.brito@gmail.com>
2014-08-19 23:10:09 +0100

    Finished the section 5.7 where you configure the admin interface for the tango_with_django_project

--- django/workspace/tango_with_django_project/tango_with_django_project/settings.py
+++ django/workspace/tango_with_django_project/tango_with_django_project/settings.py
@@ -12,6 +12,8 @@ TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 ##Static media folder path
 STATIC_PATH = os.path.join(PROJECT_PATH,'static')
 
+##Database Path
+DATABASE_PATH = os.path.join(PROJECT_PATH, 'rango.db')
 
 # Django settings for tango_with_django_project project.
 DEBUG = True
@@ -25,13 +27,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': DATABASE_PATH, # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        #'USER': '',
+        #'PASSWORD': '',
+    #'HOST': '', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        #'PORT': '', # Set to empty string for default.
     }
 }
 
@@ -130,14 +132,14 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    #'django.contrib.auth',
-    #'django.contrib.contenttypes',
-    #'django.contrib.sessions',
-    #'django.contrib.sites',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
     #'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'rango',
