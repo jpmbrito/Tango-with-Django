5fa61f050c1fe528d54094166bed3f2ba38faef0
Joao Brito <joao.mano.brito@gmail.com>
2014-10-02 15:00:21 +0100

    Finished the chapter 11 from tango with django

--- django/workspace/tango_with_django_project/rango/forms.py
+++ django/workspace/tango_with_django_project/rango/forms.py
@@ -33,13 +33,34 @@ from django.contrib.auth.models import User
 from django import forms
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(
+                    help_text="Username"
+                    )
+                    
+    email = forms.CharField(
+                    help_text="Email"
+                    )
+                    
+    password = forms.CharField(
+                    widget=forms.PasswordInput(),
+                    help_text="Password"
+                    )
     
     class Meta:
         model = User
         fields = ('username' , 'email' , 'password')
 
 class UserProfileForm(forms.ModelForm):
+    website = forms.URLField(
+                    help_text="Website",
+                    required=False
+                    )
+    
+    picture = forms.ImageField(
+                    help_text="Profile Image",
+                    required=False
+                    )
+    
     class Meta:
         model = UserProfile
         fields = ('website', 'picture')
